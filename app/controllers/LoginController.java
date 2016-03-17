aacea49e0f9a1bc636081931d5898742be9cf2f5
Kyle Thornton <kthornt2@oakland.edu>
2016-03-17 16:50:46 -0400

    4th Commit, navigation bar, honed style

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -9,7 +9,7 @@ import views.html.*;
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
-public class HomeController extends Controller {
+public class LoginController extends Controller {
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -17,9 +17,7 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-    public Result aboutUs() { return ok(aboutus.render("About Us"));}
+
+    public Result login() { return ok(login.render("Login"));}
 
 }
